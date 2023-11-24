b7a2245f7516ea0fb295d0fe8733ce505409aafe
AnaduCG <godzionwin@gmail.com>
2023-11-24 11:06:58 +0100

    Adjusted footer border

--- Frontend/main/src/Footer.jsx
+++ Frontend/main/src/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer(){
     return(
-        <div className="text-center h-10 border py-10">
+        <div className="text-center h-10 border-t py-10">
             <p className="font-bold">Porthree 2023 | All Rights Reserved</p>
         </div>
     )
