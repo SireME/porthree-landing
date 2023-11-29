ec5ecfcb95476476ecb9c5944ee8231adbf76280
Mbang Ernest N <mbangernest@yahoo.com>
2023-11-29 15:31:04 +0100

    create api routes for models

--- Porthree/main/serializers.py
+++ Porthree/main/serializers.py
@@ -1,6 +1,58 @@
 from rest_framework import serializers
 from .models import PorthreeAbout, PorthreeFAQ
+from .models import User, Skill, Social, Hero
+from .models import PostTags, Post, Comment
+from .models import Theme, Project, ProjectTools
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class PostTagsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostTags
+        fields = '__all__'
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+class SkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = '__all__'
+
+class SocialSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Social
+        fields = '__all__'
+
+class HeroSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hero
+        fields = '__all__'
+
+class ThemeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Theme
+        fields = '__all__'
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+class ProjectToolsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProjectTools
+        fields = '__all__'
 
 class PorthreeAboutSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,6 +60,7 @@ class PorthreeAboutSerializer(serializers.ModelSerializer):
         fields = ('id', 'created_at',
                     'updated_at', 'about',
                     'image')
+
 class PorthreeFAQSerializer(serializers.ModelSerializer):
     class Meta:
         model = PorthreeFAQ
--- Porthree/main/views.py
+++ Porthree/main/views.py
@@ -3,7 +3,85 @@ from rest_framework import viewsets
 from .serializers import PorthreeAboutSerializer, PorthreeFAQSerializer
 from .models import PorthreeAbout, PorthreeFAQ
 
-# Create your views here.
+# user and serializer
+from .models import User
+from .serializers import UserSerializer
+
+# posttags and serializer
+from .models import PostTags
+from .serializers import PostTagsSerializer
+
+# post and serializer
+from .models import Post
+from .serializers import PostSerializer
+
+# comments and serialiser
+from .models import Comment
+from .serializers import CommentSerializer
+
+# skill and serializer
+from .models import Skill
+from .serializers import SkillSerializer
+
+# social and serializer
+from .models import Social
+from .serializers import SocialSerializer
+
+# hero and serializer
+from .models import Hero
+from .serializers import HeroSerializer
+
+# theme and serializer
+from .models import Theme
+from .serializers import ThemeSerializer
+
+# project and serializer
+from .models import Project
+from .serializers import ProjectSerializer
+
+# projecttools and serializer
+from .models import ProjectTools
+from .serializers import ProjectToolsSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class PostTagsViewSet(viewsets.ModelViewSet):
+    queryset = PostTags.objects.all()
+    serializer_class = PostTagsSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+class SkillViewSet(viewsets.ModelViewSet):
+    queryset = Skill.objects.all()
+    serializer_class = SkillSerializer
+
+class SocialViewSet(viewsets.ModelViewSet):
+    queryset = Social.objects.all()
+    serializer_class = SocialSerializer
+
+class HeroViewSet(viewsets.ModelViewSet):
+    queryset = Hero.objects.all()
+    serializer_class = HeroSerializer
+
+class ThemeViewSet(viewsets.ModelViewSet):
+    queryset = Theme.objects.all()
+    serializer_class = ThemeSerializer
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+class ProjectToolsViewSet(viewsets.ModelViewSet):
+    queryset = ProjectTools.objects.all()
+    serializer_class = ProjectToolsSerializer
 
 class PorthreeAboutView(viewsets.ModelViewSet):
     serializer_class = PorthreeAboutSerializer
