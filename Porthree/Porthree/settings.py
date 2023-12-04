f990e24b6265242e2277e47a48c1b85b62fbb04c
AnaduCG <godzionwin@gmail.com>
2023-12-04 17:25:31 +0100

    static path usrl redefine

--- Porthree/Porthree/settings.py
+++ Porthree/Porthree/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     # os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'MainApp/static'),
-    os.path.join(BASE_DIR, 'UserApp/static'),
+    # os.path.join(BASE_DIR, 'UserApp/static'),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
