4210cffa9b254c639c903f35dfccc032a236ed61
AnaduCG <godzionwin@gmail.com>
2023-12-17 17:00:13 +0100

    Password reset templating

--- Porthree/MainApp/urls.py
+++ Porthree/MainApp/urls.py
@@ -17,7 +17,7 @@ from .views import (
 )
 
 urlpatterns = [
-    path("", index, name="index"),
+    path('', index, name="index"),
     path("signup/", signup, name="signup"),
     path("login/", user_login, name="login"),
     path("logout/", user_logout, name="logout"),
